926d347a1c9b6d2310580666433503c07dc8665d
rhcatt <154297137+rhcatt@users.noreply.github.com>
2023-12-19 11:45:10 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.czix4j33errd.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "pong-employee"
+customregion = "us-east-1"
+
